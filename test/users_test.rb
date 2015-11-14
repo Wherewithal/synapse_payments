5d1021c44b3b2bf4fcd43aa705edefe8ceaf88e8
Javier Julio <jjfutbol@gmail.com>
2015-11-13 19:26:31 -0500

    Add several assertions for all tests

--- test/users_test.rb
+++ test/users_test.rb
@@ -18,21 +18,36 @@ class UsersTest < Minitest::Test
   end
 
   def test_user_find
-    VCR.use_cassette('users_find') do
-      user = authenticated_client.users.find('5641019d86c273308e8193f1')
+    user_id = '5641019d86c273308e8193f1'
 
-      refute_predicate user[:_id], :empty?
-      refute_predicate user[:refresh_token], :empty?
+    VCR.use_cassette('users_find') do
+      user = authenticated_client.users.find(user_id)
+
+      assert_equal user_id, user[:_id]
+      assert_equal 'refresh-95452209-3b1c-4165-a4a2-021ee96cdd32', user[:refresh_token]
       assert_equal ['Javier Julio'], user[:legal_names]
     end
   end
 
-  def test_authenticate_as
-    VCR.use_cassette('users_find') do
-      user = authenticated_client.users.find('5641019d86c273308e8193f1')
+  def test_authenticate_as_and_user_client
+    user_id = '5641019d86c273308e8193f1'
+    refresh_token = 'refresh-95452209-3b1c-4165-a4a2-021ee96cdd32'
 
-      VCR.use_cassette('oauth_refresh_token') do
-        user_client = authenticated_client.users.authenticate_as(id: '5641019d86c273308e8193f1', refresh_token: user[:refresh_token])
+    VCR.use_cassette('oauth_refresh_token') do
+      user_client = authenticated_client.users.authenticate_as(id: user_id, refresh_token: refresh_token)
+
+      assert_equal refresh_token, user_client.refresh_token
+      assert_equal '1447445562', user_client.expires_at
+      assert_equal '7200', user_client.expires_in
+      assert_equal 12, user_client.refresh_expires_in
+      assert_equal 'oauth_key', user_client.oauth_key
+
+      VCR.use_cassette('users_find') do
+        user = user_client.user
+
+        assert_equal user_id, user[:_id]
+        assert_equal 'refresh-95452209-3b1c-4165-a4a2-021ee96cdd32', user[:refresh_token]
+        assert_equal ['Javier Julio'], user[:legal_names]
       end
     end
   end
